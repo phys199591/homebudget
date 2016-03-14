7635945f74be867b9f4e7507d5fca83494c18f34
adam <adam.sosnowski.86@gmail.com>
2016-03-14 23:07:25 +0100

    Testy kontrolera Expense

---
+++ HomeBudget.Test/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HomeBudget.Controllers;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+
+namespace HomeBudget.Test
+{
+    [TestClass]
+    public class ExpenseControllerTest
+    {
+        DAL.IRepository _service;
+        ExpenseController _controller;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _service = null;
+            _controller = new ExpenseController(_service);
+        }
+
+        [TestMethod]
+        public void DetailsWithIdNull()
+        {
+            var task = _controller.Details(null);
+            task.Wait();
+
+            var result = task.Result;
+
+            Assert.IsInstanceOfType(result, typeof(HttpStatusCodeResult));
+            Assert.AreEqual(((HttpStatusCodeResult)result).StatusCode, (int)System.Net.HttpStatusCode.BadRequest);
+        }
+
+        [TestMethod]
+        public void DetailsWithExistingId()
+        {
+        }
+
+        [TestMethod]
+        public void DetailsWithNotExistingId()
+        {
+            var task = _controller.Details(-1);
+            task.Wait();
+
+            var result = (ViewResult)task.Result;
+
+            Assert.AreEqual(null, result.Model);
+        }
+    }
+}
---
+++ HomeBudget/Controllers/ExpenseController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeBudget.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        DAL.IRepository service;
+
+        public ExpenseController(DAL.IRepository _service)
+        {
+            service = _service;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            return View(service.GetExpenses());
+        }
+
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id.HasValue)
+            {
+                return View(service.GetExpense(id.Value));
+            }
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        }
+
+    }
+}
